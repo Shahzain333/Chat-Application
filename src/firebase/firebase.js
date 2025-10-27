64106ab6276f81149b9bde243da2b2767b8110a0
Shahzain333 <shahzainkhannaizi1234@gmail.com>
2025-10-27 15:54:42 +0500

    Setup Projects

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_kZ5wngArhQKMEeJ6YR4P5DyJ9K2_V0M",
+  authDomain: "chatt-app-745a8.firebaseapp.com",
+  projectId: "chatt-app-745a8",
+  storageBucket: "chatt-app-745a8.firebasestorage.app",
+  messagingSenderId: "512557756637",
+  appId: "1:512557756637:web:fdb9ad9660266c91282b34"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
